9848afa7148bcabd08b7d7024404bf1d46209d04
Steve Chesnowitz <steve@chesnowitz.com>
2018-03-01 07:22:37 -0500

    backup

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,43 +1,12 @@
-if Rails.env.development?
 Sidekiq.configure_server do |config|
-  
-    config.redis = { url: 'redis://165.227.212.120/1'  }  
-    schedule_file = "config/schedule.yml"
-    if File.exists?(schedule_file)
-      Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-    end 
-  end
-  
-  Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://165.227.212.120/1'  } 
-  end
+  config.redis = { url: "redis://#{ENV['DATA_QUEUE_HOST']}:6379/5" }
+  schedule_file = "config/schedule.yml"
+  if File.exists?(schedule_file)
+    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) 
+  end 
 end
 
-if Rails.env.production?
-  Sidekiq.configure_server do |config|
-    config.redis = { url: "redis://#{ENV['DATA_REDIS_HOST']}:6379/4" } 
-    schedule_file = "config/schedule.yml"
-    if File.exists?(schedule_file)
-      Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) 
-    end 
-  end
-  
-  Sidekiq.configure_client do |config|
-    config.redis = { url: "redis://#{ENV['DATA_REDIS_HOST']}:6379/4" } 
-  end
 
-end
-
-if Rails.env.staging?
-  Sidekiq.configure_server do |config|
-    config.redis = { url: "redis://#{ENV['DATA_REDIS_HOST']}:6379/7" } 
-    schedule_file = "config/schedule.yml"
-    if File.exists?(schedule_file)
-      Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) 
-    end 
-  end
-  
-  Sidekiq.configure_client do |config|
-    config.redis = { url: "redis://#{ENV['DATA_REDIS_HOST']}:6379/7" } 
-  end
+Sidekiq.configure_client do |config|
+  config.redis = { url: "redis://#{ENV['DATA_QUEUE_HOST']}:6379/5" }
 end
